5233819419e7866a3553c1479588f6ea64ebebe2
Sergio Antón <else@MacBook-Pro-de-Sergio.local>
2019-08-20 13:07:37 +0200

    empezando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'carrito', loadChildren: './carrito/carrito.module#CarritoPageModule' },
+  { path: 'categorias', loadChildren: './categorias/categorias.module#CategoriasPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'ordenes', loadChildren: './ordenes/ordenes.module#OrdenesPageModule' },
+  { path: 'ordenes-detalle', loadChildren: './ordenes-detalle/ordenes-detalle.module#OrdenesDetallePageModule' },
+  { path: 'por-categorias', loadChildren: './por-categorias/por-categorias.module#PorCategoriasPageModule' },
+  { path: 'producto', loadChildren: './producto/producto.module#ProductoPageModule' },
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'tabs', loadChildren: './paginas/tabs/tabs.module#TabsPageModule' },
 ];
 
 @NgModule({
