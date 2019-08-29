3870973f6ff58cb1d81778b6ff96ef09d59590e7
Eugenio-PC <sergio@elsesystems.es>
2019-08-29 08:19:13 +0200

    prueba git

--- src/app/paginas/producto/producto.page.ts
+++ src/app/paginas/producto/producto.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RutaImagenPipe } from '../../pipes/ruta-imagen.pipe';
+import { CarritoService } from '../../servicios/carrito.service';
 
 @Component({
   selector: 'app-producto',
@@ -11,7 +12,7 @@ export class ProductoPage implements OnInit {
 
   producto:any;
   
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private _cs:CarritoService) {
     this.route.queryParams.subscribe((respuesta)=>{
       this.producto = JSON.parse(respuesta.producto)
     })
--- src/app/servicios/carrito.service.ts
+++ src/app/servicios/carrito.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { present } from '@ionic/core/dist/types/utils/overlays';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarritoService {
 
-  constructor() { }
+  productos:any[] = [];
+
+  constructor(public alert: AlertController) { }
+
+  agregar_carrito(producto:any){
+    //Vamos a comprobar que el producto no exista en el carrito
+    console.log(producto);
+    for (let item of this.productos){
+      if(item.codigo == producto.codigo){
+        //Mensaje que existe ya en el carrito
+        this.presentAlert(producto);
+        return;
+      }
+    }
+    this.productos.push(producto);//Vamos insertando al carrito
+  }
+
+  async presentAlert(producto){
+    const alert = await this.alert.create({
+      header: "Item existe",
+      subHeader: producto.producto + ", ya se encuentra en el carrito",
+      buttons: ["OK"]
+    });
+    await alert.present();
+  }
 }
