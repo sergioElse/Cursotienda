1d0265dbc252969a83cd88ae1022601222a46d36
Eugenio-PC <sergio@elsesystems.es>
2019-08-30 19:22:01 +0200

    Me voy a tomar algo

--- src/app/paginas/login/login.page.ts
+++ src/app/paginas/login/login.page.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  correo:string = "";
+  contrasena:string = ""; 
+
   constructor() { }
 
   ngOnInit() {
